fd58d834bbdcb134320e84b3d06d13faffba9ddd
chitra7928@gmail.com <chitra7928@gmail.com>
2022-01-28 14:16:42 +0530

    test case 004 automated

--- src/gitpractice/GitBash.java
+++ src/gitpractice/GitBash.java
@@ -12,6 +12,7 @@ public class GitBash {
 public static void main(String[] args) {
 	System.out.println("*************hELLO*************");
 	System.out.println("*************thisis *************");
+	System.out.println("*****3 rd change****");
 }
 
 }
---
+++ src/gitpractice/Testcase004.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Testcase004 {
+	public static void main(String[] args) {
+		System.out.println("***1st commit");
+	}
+
+}
