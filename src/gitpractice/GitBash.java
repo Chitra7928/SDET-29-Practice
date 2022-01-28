439b2236370d499bc5e992dfdeff3c23af768bbd
chitra7928@gmail.com <chitra7928@gmail.com>
2022-01-28 13:53:59 +0530

    Testcase 2 automated

--- src/gitpractice/GitBash.java
+++ src/gitpractice/GitBash.java
@@ -11,6 +11,7 @@ public class GitBash {
 	
 public static void main(String[] args) {
 	System.out.println("*************hELLO*************");
+	System.out.println("*************thisis *************");
 }
 
 }
