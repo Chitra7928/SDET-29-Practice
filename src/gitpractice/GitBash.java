9ea85a3a17ffccadc8da7c66235bb212f18b9949
chitra7928@gmail.com <chitra7928@gmail.com>
2022-01-28 13:23:07 +0530

    This is my first java class

---
+++ src/gitpractice/GitBash.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package gitpractice;
+
+/**
+ * @author NEW
+ *
+ */
+public class GitBash {
+	
+public static void main(String[] args) {
+	System.out.println("*************hELLO*************");
+}
+
+}
